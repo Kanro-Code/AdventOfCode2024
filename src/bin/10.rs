5016c3a41d2a64ffa0b6db58ca50252429e7f0d7
Thijs ter Velde <tervelde.thijs@gmail.com>
2025-04-19 17:44:16 +0200

    Day 10, Part 2 - Return functionality for part 1

--- src/bin/10.rs
+++ src/bin/10.rs
@@ -1,6 +1,14 @@
 use advent_of_code::{Direction, Grid, Point};
+use std::collections::HashMap;
 
 advent_of_code::solution!(10);
+
+const DIRECTIONS: [Direction; 4] = [
+    Direction::North,
+    Direction::South,
+    Direction::East,
+    Direction::West,
+];
 
 pub fn part_one(input: &str) -> Option<u64> {
     let input = parse_input(input);
@@ -15,32 +23,31 @@ pub fn part_one(input: &str) -> Option<u64> {
     Some(total as u64)
 }
 
-pub fn part_two(_input: &str) -> Option<u64> {
-    None
+pub fn part_two(input: &str) -> Option<u64> {
+    let input = parse_input(input);
+
+    let total: usize = input.iter().with_points().filter_map(|(point, v)| {
+        if v == 0 {
+            let count = walk_path(&input, point);
+            return Some(count);
+        }
+        None
+    }).sum();
+    Some(total as u64)
 }
 
 pub fn walk_path(grid: &Grid<u32>, start: Point) -> usize {
-    let mut found = Vec::new();
+    let mut found = HashMap::new();
     let current = grid.get(start);
 
     walk_path_recur(grid, start, current, &mut found);
 
-    found.len()
+    found.values().sum()
 }
 
-const DIRECTIONS: [Direction; 4] = [
-    Direction::North,
-    Direction::South,
-    Direction::East,
-    Direction::West,
-];
-
-pub fn walk_path_recur(grid: &Grid<u32>, point: Point, current: u32, found: &mut Vec<Point>) {
+pub fn walk_path_recur(grid: &Grid<u32>, point: Point, current: u32, found: &mut HashMap<Point, usize>) {
     if current == 9 {
-        if !found.contains(&point) {
-            found.push(point);
-        }
-        return;
+        *found.entry(point).or_insert(0) += 1
     }
 
     DIRECTIONS.iter().for_each(|direction| {
@@ -73,6 +80,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(81));
     }
 }
